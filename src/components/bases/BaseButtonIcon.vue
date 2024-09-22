788777934ef791454b2c113417c40cc9d59076de
GDK1995 <87063068+GDK1995@users.noreply.github.com>
2024-09-22 21:12:51 +0600

    order page list

---
+++ src/components/bases/BaseButtonIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="border rounded p-2">
+    <slot></slot>
+  </button>
+</template>
--- src/components/main/order/TheOrder.vue
+++ src/components/main/order/TheOrder.vue
@@ -1,11 +1,31 @@
 <script setup>
+import { TrashIcon } from '@heroicons/vue/24/solid'
+import BaseButtonIcon from '../../bases/BaseButtonIcon.vue'
+
+// props
 const props = defineProps({
   orderList: Array
 })
 </script>
 
 <template>
-  <div>
-    {{ props.orderList }}
+  <div class="flex flex-col text-center">
+    <p class="text-2xl my-8">Заказ</p>
+    <div class="flex justify-center">
+      <ul class="flex flex-col w-4/5 m-4">
+        <li v-for="(item, index) in props.orderList" :key="item" class="flex">
+          <div class="flex justify-between py-2 px-4 border flex-grow">
+            <p>{{index + 1}}) {{ item.title }} <span v-if="item.excludedIngredients">(исключить: {{ item.excludedIngredients }})</span></p>
+            <div class="flex">
+              <p class="mr-8">{{ item.price }} x {{ item.count }}</p>
+              <p>{{ item.price * item.count }}</p>
+            </div>
+          </div>
+          <BaseButtonIcon class="ml-4">
+            <TrashIcon class="h-5 text-red-500"/>
+          </BaseButtonIcon>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
